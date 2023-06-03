bf91c5a18da0b0fb4db64a41cfa4edc955c4af5a
Christopher Torgalson <manager@bedlamhotel.com>
2023-06-03 17:56:52 +0200

    Adds proper handling for contents to permit no contents to be passed to class

--- web/js/disclosure.js
+++ web/js/disclosure.js
@@ -48,11 +48,9 @@ class Disclosure {
    *
    * @constructor
    */
-  constructor(trigger, contents, settings = {}) {
+  constructor(trigger, contents = undefined, settings = {}) {
     this.trigger = trigger;
-    this.contents = typeof contents[Symbol.iterator] === 'function'
-      ? [...contents]
-      : [contents];
+    this.contents = this._getContents(contents);
     this.settings = {...this.defaults, ...settings};
     this.defaultExpanded = this.trigger.getAttribute('aria-expanded') === 'true';
     this.trigger.disclosure = this;
@@ -95,6 +93,36 @@ class Disclosure {
     bubbles: true,
     detail: {action: type}
   });
+
+  /**
+   * Ensures this.contents is an array, whatever was passed to the constructor.
+   *
+   * @param {(Object[]|Object|undefined)} contents
+   *   The contents variable, if any, that was passed into the constructor. May
+   *   be an Object (if it's the result of a call to .querySelectorAll() or to
+   *   .querySelector()), an Object[] (if the constructor has been passed an
+   *   array), or undefined if nothing at all has been passed in.
+   * @return {Object[]} contentsList
+   *   The array of elements to use as contents in the disclosure. If we've
+   *   received a NodeList, we convert it to an array; if we've recieved an
+   *   Element, we add it to an array; if we've received nothing at all, we
+   *   add the HTML element immediately following this.trigger to an array.
+   * @protected
+   * @method
+   */
+  _getContents = (contents) => {
+    let contentsList;
+
+    if (typeof contents !== 'undefined') {
+      contentsList = typeof contents[Symbol.iterator] === 'function'
+        ? [...contents]
+        : [contents]
+    } else {
+      contentsList = [this.trigger.nextElementSibling];
+    }
+
+    return contentsList;
+  };
 
   /**
    * Handles click events on this.trigger.
