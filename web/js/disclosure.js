de4090fc431518da777c2b1cec331a699ca903d7
Christopher Torgalson <manager@bedlamhotel.com>
2023-06-03 13:43:21 +0200

    Defines new members and methods related to the public api

--- web/js/disclosure.js
+++ web/js/disclosure.js
@@ -7,6 +7,16 @@
 class Disclosure {
 
   /**
+   * @member {boolean}
+   */
+  _enabled;
+
+  /**
+   * @member {boolean}
+   */
+  _expanded;
+
+  /**
    * @member {Object}
    */
   trigger;
@@ -18,7 +28,70 @@ class Disclosure {
    */
   constructor(trigger) {
     this.trigger = trigger;
+    this.trigger.disclosure = this;
     console.log('Instantiated new Disclosure object.');
   }
+
+  /**
+   * Creates a custom disclosure event.
+   *
+   * @return {Object}
+   *   A custom event to be dispatched by the object.
+   * @protected
+   * @method
+   */
+  _createEvent = (type) => null;
+
+  /**
+   * Collapses the disclosure.
+   *
+   * @public
+   * @method
+   */
+  collapse = () => null;
+
+  /**
+   * Disables the disclosure.
+   *
+   * @public
+   * @method
+   */
+  disable = () => null;
+
+  /**
+   * Enables the disclosure.
+   *
+   * @public
+   * @method
+   */
+  enable = () => null;
+
+  /**
+   * Expands the disclosure.
+   *
+   * @public
+   * @method
+   */
+  expand = () => null;
+
+  /**
+   * Returns _enabled status of this disclosure.
+   *
+   * @return {boolean}
+   *   The value of this._enabled.
+   * @public
+   * @method
+   */
+  isEnabled = () => this._enabled;
+
+  /**
+   * Returns expanded status of this disclosure.
+   *
+   * @return {boolean}
+   *   The value of this._expanded.
+   * @public
+   * @method
+   */
+  isExpanded = () => this._expanded;
 
 }
